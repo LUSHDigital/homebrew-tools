a3af527fb4349851f7e3b3c06933d81f379a539d
Benjamin Cable <ben.cable@lush.co.uk>
2018-03-13 14:43:32 +0000

    bump modelgen

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,8 +1,8 @@
 class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
-  url "https://github.com/LUSHDigital/modelgen/archive/1.1.5.tar.gz"
-  sha256 "aa03458c52d44822b396033a59490f1f0db7bbfb687781e5111e8f2a3eec338c"
+  url "https://github.com/LUSHDigital/modelgen/archive/1.1.6.tar.gz"
+  sha256 "91eacd0baad4091eeb7979bf162d048095593ed88e13d3f0b681527717587b10"
 
   depends_on "go" => :build
 
