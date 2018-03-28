6c2ab59d6b6b29f0767c7e149e96206266e5538d
Benjamin Cable <ben.cable@lush.co.uk>
2018-03-28 13:22:04 +0100

    bumped modelgen -> issued patch

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,8 +1,8 @@
 class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
-  url "https://github.com/LUSHDigital/modelgen/archive/1.2.0.tar.gz"
-  sha256 "6e304289c36951a21eb6890e4bdf63150bdf2f5c9c418442ec54b8f24174e4bf"
+  url "https://github.com/LUSHDigital/modelgen/archive/1.2.1.tar.gz"
+  sha256 "25a523eb0e1011ac6f1a77eac6d73c61663404973f020ee53e574ec4d5950bca"
 
   depends_on "go" => :build
 
