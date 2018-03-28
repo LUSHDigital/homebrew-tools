4b2f5909a1bca94de8901bf78b1fe16ed5e7461a
Benjamin Cable <ben.cable@lush.co.uk>
2018-03-28 13:36:02 +0100

    update sha

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -2,7 +2,7 @@ class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
   url "https://github.com/LUSHDigital/modelgen/archive/1.2.1.tar.gz"
-  sha256 "25a523eb0e1011ac6f1a77eac6d73c61663404973f020ee53e574ec4d5950bca"
+  sha256 "9a4fa6acb40486ad85861f30ef51e28d6c6172207f71b3a077063d05412c3995"
 
   depends_on "go" => :build
 
