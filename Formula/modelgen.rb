922821f6c90ee7087c24578db6756240eeeaa3b8
codingconcepts <bassrob@gmail.com>
2018-04-03 10:50:06 +0100

    Added support for explicit nulls for NullTime in JSON

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,8 +1,8 @@
 class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
-  url "https://github.com/LUSHDigital/modelgen/archive/1.3.0.tar.gz"
-  sha256 "a4aa7a54f5144df8d7dd98f83a31194b921b59aad1338a84864b3a93123c32fc"
+  url "https://github.com/LUSHDigital/modelgen/archive/1.3.1.tar.gz"
+  sha256 "f0543d8282d21f27b113388caaa37cf83498f26cadf642ca0eab481c7db89d3a"
 
   depends_on "go" => :build
 
