8ca149719d43be155a35637673447370a85d9535
codingconcepts <bassrob@gmail.com>
2018-04-04 09:30:36 +0100

    Reduced chance for naming collisions between Queryer and receiver names

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,8 +1,8 @@
 class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
-  url "https://github.com/LUSHDigital/modelgen/archive/1.3.1.tar.gz"
-  sha256 "f0543d8282d21f27b113388caaa37cf83498f26cadf642ca0eab481c7db89d3a"
+  url "https://github.com/LUSHDigital/modelgen/archive/1.3.2.tar.gz"
+  sha256 "9075eceabb1cddf3cf13cc86fb6d18933bff297970b2d880fc5e37932524d9c4"
 
   depends_on "go" => :build
 
