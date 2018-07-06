43528fd8706e0417984e76738dc502dcfd7cc71f
Dan Richards <dan.richards@lush.co.uk>
2018-07-06 10:03:39 +0100

    pdated modelgen to v1.3.7

--- Formula/modelgen.rb
+++ Formula/modelgen.rb
@@ -1,8 +1,8 @@
 class Modelgen < Formula
   desc "Generate models from reading your database"
   homepage "https://github.com/LUSHDigital/modelgen"
-  url "https://github.com/LUSHDigital/modelgen/archive/1.3.6.tar.gz"
-  sha256 "218ca78a3bbace5f059737c866540674cfe0d54dfcc71fc96130a8d11dca790f"
+  url "https://github.com/LUSHDigital/modelgen/archive/1.3.7.tar.gz"
+  sha256 "aa866f12d4f1d3b0a4a826d33d741a460f6ace949b452e9836ee3ec5a0d2238e"
 
   depends_on "go" => :build
 
